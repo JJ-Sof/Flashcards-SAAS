8faddf5ebad18407d35344c891a5f903b6fc01d9
apurva-honrao <apurvahonrao@gmail.com>
2024-08-15 21:08:00 +0530

    logo

--- app/components/Header.js
+++ app/components/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
                 color: "#F0F1FA",
               }}
             >
-              ◆smartflash
+              ◆SmartFlash
             </Typography>
           </Box>
           {/* Authentication Buttons */}
